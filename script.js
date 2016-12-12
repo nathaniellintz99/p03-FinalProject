c7744002688f92b073729f41fa9e2d93c723e585
Nathaniel Lintz <nlintz@mail.csuchico.edu>
2016-12-12 00:59:18 -0800

    lib midnight fml

--- script.js
+++ script.js
@@ -1,57 +1,63 @@
 // Nathaniel Lintz: Project 3
 
-//soundcloud embed urls, did not use s.c. api because it gave me errors I could not figure out so stuck with the automated sc embeding
-var abc = {url:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/265040990&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true", songTitle:"Back Where I Belong" };
-var url = ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/265040990&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true",
-"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/160263594&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true",
-"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/136515969&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true"];
-var song = ["Back Where I Belong", "Feel The Volume", "Miami 82 (Kygo Remix)"];
-var songPage = ["https://soundcloud.com/otto-knows/otto-knows-ft-avicii-back-where-i-belong", "https://soundcloud.com/jauzofficial/feelthevolume", "https://soundcloud.com/kygo/syn-cole-miami-82-kygo-remix"];
-var artist = ["Otto Knows ft. Avicii", "Jauz", "Syn Cole"];
-var artistPage = ["https://soundcloud.com/otto-knows", "https://soundcloud.com/jauzofficial", "https://soundcloud.com/kygo"];
-var results = song1;
-var i = results;
-var fullList = [song1];
-var song1 = {song:"Back Where I Belong", artist:"Otto Knows ft. Avicii", songUrl:"https://soundcloud.com/otto-knows/otto-knows-ft-avicii-back-where-i-belong", artistUrl:"https://soundcloud.com/otto-knows", embed:"https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/265040990&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true"};
+window.onload = storeSongs;
+// Global variables..
+var quiz, song=[], songPage=[], artist=[], artistPage=[], embed=[];
 
-function loadResults(){
-  document.getElementById("myFrame").src = song1.embed;
-  document.getElementById("song").innerHTML = song1.song;
-  document.getElementById("songPage").href = song1.songUrl;
-  document.getElementById("artist").innerHTML = song1.artist;
-  document.getElementById("artistPage").href = song1.artistUrl;
+function storeSongs(){
+  song = ["Back Where I Belong", "Feel The Volume", "Miami 82 (Kygo Remix)"];
+  songPage = ["https://soundcloud.com/otto-knows/otto-knows-ft-avicii-back-where-i-belong", "https://soundcloud.com/jauzofficial/feelthevolume", "https://soundcloud.com/kygo/syn-cole-miami-82-kygo-remix"];
+  artist = ["Otto Knows ft. Avicii", "Jauz", "Syn Cole"];
+  artistPage = ["https://soundcloud.com/otto-knows", "https://soundcloud.com/jauzofficial", "https://soundcloud.com/kygo"];
+  embed = ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/265040990&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true",
+  "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/160263594&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true",
+  "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/136515969&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=true"];
+
 }
-
-function loadList(){
-  document.getElementById("myFrame0").src = url[0];
-  document.getElementById("myFrame1").src = url[1];
-  document.getElementById("myFrame2").src = url[2];
-
-
-
-  document.getElementById("myFrame").src = url[i];
+function loadResults(){
+  var i = 2;
+  document.getElementById("myFrame").src = embed[i];
   document.getElementById("song").innerHTML = song[i];
   document.getElementById("songPage").href = songPage[i];
   document.getElementById("artist").innerHTML = artist[i];
   document.getElementById("artistPage").href = artistPage[i];
 }
 
-
-var questionA = [];
-
-function checkA(){
-  questionA[0] = "A";
-  document.getElementById("verdictA").classList.add("button1True");
-  document.getElementById("verdictB").classList.remove("button1");
-}
-function checkB(){
-  questionA[0] = "B";
-  document.getElementById("verdictB").classList.add("button1True");
-  document.getElementById("verdictBd").classList.remove("button1");
-}
-
-
-function addStyle(){
-  if(document.getElementById('buttonA').clicked == false || document.getElementById('buttonB').clicked == false){
+function loadList(){
+  for (var i = 0; i < song.length; i++){
+    document.getElementById("myFrame" + i).src = embed[i];
+    document.getElementById("song" + i).innerHTML = song[i];
+    document.getElementById("songPage" + i).href = songPage[i];
+    document.getElementById("artist" + i).innerHTML = artist[i];
+    document.getElementById("artistPage" + i).href = artistPage[i];
   }
+}
+function check(){
+    var answers = document.getElementById("q1");
+    x.checked = true;
+
+
+    window.location = 'results.html';
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
