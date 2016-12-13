9a0f832847428c87eb2d8bdb48705bc7789be2ce
LOKII2\nathaniellintz99 <nathaniellintz99@gmail.com>
2016-12-12 17:40:05 -0800

    no message

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ function check(){
       var a4 = document.getElementById("q4").value;
       var a5 = document.getElementById("q5").value;
       var a6 = document.getElementById("q6").value;
-      document.getElementById("go").classList.remove("visibility");
       if(a1.value == "energy" && a5.value=="stargaze"){
+        document.getElementById("go").classList.remove("visibility");
         songNumber = 2;
         setCookie("quiz", songNumber, 7);
       }
